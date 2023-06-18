01a727005031b60d35273f4602521ea8ae8d663a
hadi-darwish <hadi.darwish.03@gmail.com>
2023-06-19 01:47:42 +0300

    📄models ➡️ adding cv model

---
+++ CVManagementWebApp/Models/CV.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CVManagementWebApp.Models
+{
+    public class CV
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string FirstName { get; set; }
+        [Display(Name = "First Name")]
+        [Required]
+        public string LastName { get; set; }
+        [Display(Name = "Last Name")]
+        [Required]
+        public DateTime DateOfBirth { get; set; }
+        [Display(Name = "Date Of Birth")]
+        [Required]
+        public string Nationality { get; set; }
+        [Required]
+        public string Gender { get; set; }
+        [Required]
+        public Array ProgrammingSkills { get; set; }
+        [Display(Name = "Programming Skills")]
+        [Required]
+        public EmailAddressAttribute Email { get; set; }
+        [Required]
+        public IFormFile Photo { get; set; }
+    }
+}
