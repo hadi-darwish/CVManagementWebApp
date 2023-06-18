d351a3ac95def1a624889365a6539563c1f52df2
hadi-darwish <hadi.darwish.03@gmail.com>
2023-06-19 01:14:16 +0300

    🛣️ routes ➡️ adding basic routes

---
+++ CVManagementWebApp/Pages/BrowseCVs.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CVManagementWebApp.Pages
+{
+    public class BrowseCVsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ CVManagementWebApp/Pages/SendCV.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CVManagementWebApp.Pages
+{
+    public class SendCVModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
